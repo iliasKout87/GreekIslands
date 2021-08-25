12d862040378c88a3c9395b64dc71840cdc0b05b
ilias koutlas <koutlas@semitron.gr>
2021-08-25 17:37:15 +0300

    Added Nisyros

---
+++ GreekIslands/Nisyros.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreekIslands
+{
+    public class Nisyros : GreekIsland
+    {
+        public override string TellGeneralImpression() => "Rekla, bafos, zesti kai fasaiismos";
+    }
+}
