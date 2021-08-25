f536b74b34193d07c8b016a1f53c1779a045426f
ilias koutlas <koutlas@semitron.gr>
2021-08-25 17:02:47 +0300

    Added Amorgos

---
+++ GreekIslands/Amorgos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreekIslands
+{
+    public class Amorgos : GreekIsland
+    {
+        public override string TellGeneralImpression() => "Very beautiful island. Nice people.";
+    }
+}
