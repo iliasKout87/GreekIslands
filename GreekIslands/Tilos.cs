f6d97c82018fe6d2854a5c8cb920a3c4f8c660a6
ilias koutlas <koutlas@semitron.gr>
2021-08-25 17:08:49 +0300

    Added Tilos

---
+++ GreekIslands/Tilos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreekIslands
+{
+    public class Tilos : GreekIsland
+    {
+        public override string TellGeneralImpression() => "Beatiful island. Something very nice in the air. Eristos is a great beach.";
+    }
+}
