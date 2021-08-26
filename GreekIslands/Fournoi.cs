0511b95ca4d5c3e51b2a5feffdeb6ca6e8be9cac
SEMITRON\koutlas <koutlas@semitron.gr>
2021-08-26 11:03:21 +0300

    Added fournoi

---
+++ GreekIslands/Fournoi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreekIslands
+{
+    public class Fournoi : GreekIsland
+    {
+        public override string TellGeneralImpression() => "Nice small island. Nice fish, beautiful beaches";
+    }
+}
