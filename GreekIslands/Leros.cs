b5b29a9e6e8480a7a40cc5fc3d653ec0d86b99a9
ilias koutlas <koutlas@semitron.gr>
2021-08-26 10:50:17 +0300

    Added Leros

---
+++ GreekIslands/Leros.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreekIslands
+{
+    public class Leros : GreekIsland
+    {
+        public override string TellGeneralImpression() => "Etsi ki etsi. Polles koprogates.";
+    }
+}
